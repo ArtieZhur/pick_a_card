567943be7dbee6eb0a77d2367289a85b99241f71
Artie <123793154+ArtieZhur@users.noreply.github.com>
2023-01-28 08:34:10 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -3,4 +3,5 @@ values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 # Буби, черви, крести, пики.
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
 
+#Выводим на экран случайные значение и масть
 puts "#{values.sample} of #{suits.sample}"
